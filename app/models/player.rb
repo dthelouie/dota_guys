6a95933860f5915610151d07f97524aa286b434c
David Louie <dlouie1492@gmail.com>
2016-08-20 04:07:48 -0400

    Add Kill-Death-Assist Ratio column

--- app/models/player.rb
+++ app/models/player.rb
@@ -6,8 +6,14 @@ class Player < ActiveRecord::Base
   serialize :items
 
 
+  def kda
+    ratio = (self.kills.to_f + self.assists.to_f) / self.deaths.to_f
+    ratio.round(1)
+  end
+
+
   def abandoned_or_not_connected?
     status != 'played'
   end
-  
+
 end
