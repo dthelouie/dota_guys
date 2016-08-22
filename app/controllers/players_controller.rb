89a5f512d353dc950de019db64563f4e29450d30
David Louie <dlouie1492@gmail.com>
2016-08-22 19:08:45 -0400

    Increase number of loaded games, change timestamp to local time

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,7 +5,7 @@ class PlayersController < ApplicationController
     player_data = SteamWebApi::Player.new(params[:player_id].to_i + 76561197960265728)
     player_profile = player_data.summary.profile
     @player = User.find_or_create_by(uid: (player_profile['steamid'].to_i - 76561197960265728).to_s, nickname: player_profile['personaname'], avatar_url: player_profile['avatarmedium'], profile_url: player_profile['profileurl'])
-    @player.load_matches!(5)
+    @player.load_matches!(10)
 
     @matches = @player.matches.order('started_at DESC')
 
