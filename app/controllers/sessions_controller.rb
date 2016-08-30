6ec2eda816538c668d8c607d4f170bcb6becf83b
David Louie <dlouie1492@gmail.com>
2016-08-30 13:17:39 -0400

    Remove rescue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,13 @@ class SessionsController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: :create
 
   def create
-    begin
       @user = User.from_omniauth request.env['omniauth.auth']
-    rescue
-      flash[:error] = "NOT GONNA BE ABLE TO DO IT"
-    else
+    if @user
       @user.load_matches!(10)
       session[:user_id] = @user.id
-      flash[:success] = "HEY #{@user.nickname}!"
+      flash[:success] = "HEY #{@user.nickname}!")
+    else
+      flash[:error] = "NOT GONNA BE ABLE TO DO IT"
     end
     redirect_to root_path
   end
