7f8a02f9b6465caec2f1f631d1d670788a4887fb
David Louie <dlouie1492@gmail.com>
2016-08-30 16:57:11 -0400

    Add search feature

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,13 +1,22 @@
 class PlayersController < ApplicationController
 
   def show
-    redirect_to root_path if params[:player_id].to_s == current_user.uid
-    player_data = SteamWebApi::Player.new(params[:player_id].to_i + 76561197960265728)
+    redirect_to root_path if current_user && params[:player_id].to_s == current_user.uid
+    if params[:player_id].to_s.length != 17
+      id = params[:player_id].to_i + 76561197960265728
+    else
+      id = params[:player_id].to_i
+    end
+    player_data = SteamWebApi::Player.new(id)
     player_profile = player_data.summary.profile
     @player = User.find_or_create_by(uid: (player_profile['steamid'].to_i - 76561197960265728).to_s, nickname: player_profile['personaname'], avatar_url: player_profile['avatarmedium'], profile_url: player_profile['profileurl'])
-    @player.load_matches!(10)
 
-    @matches = @player.matches.order('started_at DESC')
+    if @player
+      @player.load_matches!(10)
+      @matches = @player.matches.order('started_at DESC')
+    else
+      render :player_not_found
+    end
 
   end
 
